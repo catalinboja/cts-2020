0223ba699a2bf1da35e8afe97017b9382b5546ea
Catalin <catalin.boja@hotmail.com>
2020-03-23 10:57:40 +0200

    Varianta initiala seminar 6 - grupa 1074

---
+++ 1074/Seminar6/src/ro/ase/csie/cts/g1074/seminar6/Student.java
@@ -0,0 +1,60 @@
+package ro.ase.csie.cts.g1074.seminar6;
+
+import java.util.ArrayList;
+
+import ro.ase.csie.cts.g1074.seminar6.exceptii.ExceptieValoriStudent;
+
+/*
+ * varsta admisa este cuprinsa in intervalul [14, 90]
+ * numele trebuie sa contina minim 2 caractere si sa NU contina cifre
+ * notele sunt valori intregi cuprinse in intervalul [1, 10]
+ * media trebuie sa fie trunchiata la 2 zecimale
+ * 
+ * pentru valori invalide metodele genereaza o exceptie 
+ * 	de tip ExceptieValoriStudent
+ */
+
+public class Student {
+	int varsta;
+	String nume;
+	ArrayList<Integer> note;
+	
+	public Student(int varsta, String nume, ArrayList<Integer> note) {
+		super();
+		this.varsta = varsta;
+		this.nume = nume;
+		this.note = note;
+	}
+
+	public int getVarsta() {
+		return varsta;
+	}
+
+	public void setVarsta(int varsta) {
+		this.varsta = varsta;
+	}
+
+	public String getNume() {
+		return nume;
+	}
+
+	public void setNume(String nume) {
+		this.nume = nume;
+	}
+
+	public int getNota(int index) {
+		return note.get(index);
+	}
+	
+	public int getNrNote() {
+		return note.size();
+	}
+
+	public void setNote(ArrayList<Integer> note) {
+		this.note = note;
+	}
+	
+	public float getMedie2Zecimale() throws ExceptieValoriStudent{
+		return 0;
+	}
+}
